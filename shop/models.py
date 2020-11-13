62fb201846583641b74bf0ec08fbd418d32b98e6
ricarcoti <ricardo@pop-os.localdomain>
2020-11-12 21:55:17 -0500

    changed tshirt image folder

--- shop/models.py
+++ shop/models.py
@@ -75,7 +75,7 @@ class Size(models.Model):
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join('media', filename)
+    return os.path.join('tshirts', filename)
 
 
 class Product(models.Model):
