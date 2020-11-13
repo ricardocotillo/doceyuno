054790940226922239c1c55141ff43532e4bb54d
ricarcoti <ricardo@pop-os.localdomain>
2020-11-12 19:33:05 -0500

    fixed for initial setup

--- shop/forms.py
+++ shop/forms.py
@@ -7,11 +7,14 @@ class TshirtForm(forms.ModelForm):
         fields = ['name', 'description', 'price', 'default_color', 'colors']
 
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
-
-PRODUCT_SIZES_CHOICES = [(s.symbol, s.name) for s in list(Size.objects.all())]
-
 class CartAddProductForm(forms.Form):
+    PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
+    PRODUCT_SIZES_CHOICES = []
+    try:
+        PRODUCT_SIZES_CHOICES = [(s.symbol, s.name) for s in list(Size.objects.all())]
+    except:
+        pass
+
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, widget=forms.NumberInput(attrs={'class': 'form-control'}))
     size = forms.ChoiceField(choices=PRODUCT_SIZES_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
     color = forms.CharField(widget=forms.HiddenInput)
