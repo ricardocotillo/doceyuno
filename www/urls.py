6098b027d6ba554b4ca3c2c5e348120c9ee694c0
rickmaster2008 <ricardo.cotillo@gmail.com>
2021-10-18 20:33:27 -0500

    added service worker

--- www/urls.py
+++ www/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import HomeView, home
 
 urlpatterns = [
     path('', home, name='home'),
+    path('serviceworker', TemplateView.as_view(template_name='www/serviceworker.js', content_type='application/javascript'), name='serviceworker')
 ]
